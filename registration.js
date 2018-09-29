03fc52291753fbd6e8163567ef7072a5fa6ce967
zachkohl <zach.kohl@gmail.com>
2018-09-29 08:02:39 -0700

    fixed registration

--- registration.js
+++ registration.js
@@ -21,6 +21,11 @@ function routes(config) {
     })
 
     app.post('/registration', checkSession, function (req, res) {
+        if(!req.body.adultNames || !req.body.childNames){
+            res.send('please fill in at least one adult and one child name')
+        }
+        else{
+        console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
         adultNames = req.body.adultNames.split(',');
         childNames = req.body.childNames.split(',');
 
@@ -28,23 +33,25 @@ function routes(config) {
         //prep for crazy callback awesomeness
         let adultIDs = [];
         let childIDs = [];
-        let InsertAdultCounter = 1;
-        let InsertChildCounter = 1;
-        
+        let InsertAdultCounter = 0;
+        let InsertChildCounter = 0;
 
         for (i = 0; i < adultNames.length; i++) {
-            let text = 'INSERT INTO adult (adult_name) VALUES ($1) ON CONFLICT (adult_name) DO UPDATE SET adult_name = $1 G *RETURNIN'
+            let text = 'INSERT INTO adult (adult_name) VALUES ($1) ON CONFLICT (adult_name) DO UPDATE SET adult_name = $1 RETURNING *'
             let values = [adultNames[i]];
             query(text, values, callback);
             function callback(data) {
-
                 if (InsertAdultCounter < adultNames.length) {
                     adultIDs.push(data.rows[0].adult_id);
                     InsertAdultCounter++;
+                    
+                    if(InsertAdultCounter == adultNames.length){
+                        step2();
+                    }
+                   
+
                 }
-                else {
-                    step2();
-                }
+              
             };//end call back  
         };//end for loop
 
@@ -61,10 +68,12 @@ function routes(config) {
                     if (InsertChildCounter < childNames.length) {
                         childIDs.push(data.rows[0].child_id);
                         InsertChildCounter++;
-
+                        if(InsertChildCounter == childNames.length){
+                            step3();
+                        }
                     }
                     else {
-                        step3();
+                      //  step3();
                     };//end else
                 };//end callback
             };//end for
@@ -72,19 +81,14 @@ function routes(config) {
 
         function step3() {
             console.log('completed child database updates')
-            let step3Counter = 2;
-            let AdultAndChildNum = adultIDs.length + childIDs.length
-            // console.log(adultIDs);
-            // console.log(childIDs);
-            // console.log('childIDs Length: ' + childIDs.length)
+            let step3Counter = 0;
+            let AdultAndChildNum = adultIDs.length * childIDs.length;
+        
             for (let i = 0; i < adultIDs.length; i++) {
-              //  console.log('ADULT ID:  ' + adultIDs[i]);
+              
 
                 for (j = 0; j < childIDs.length; j++) {
-                    // console.log('childIDsLength: '+childIDs.length)
-                    // console.log('J: ' + j)
-                  //  console.log(Array.isArray(childIDs))
-                    console.log('CHILDs[j]:  ' + childIDs[j]);
+  
                     // Below is how one does a many to many insertion
                     let text = 'INSERT INTO adult_child (adult_id, child_id) SELECT $1, $2 WHERE NOT EXISTS (SELECT * FROM adult_child WHERE adult_id = $1 AND child_id = $2)';
                     let values = [adultIDs[i], childIDs[j]];
@@ -92,10 +96,11 @@ function routes(config) {
                     function callback(data) {
                         if (step3Counter < AdultAndChildNum) {
                             step3Counter++;
+                            if(step3Counter == AdultAndChildNum){
+                                step4();
+                            }
                         }
-                        else {
-                            step4();
-                        }
+                
                     }//end call back
 
                 }//end child for loop
@@ -107,6 +112,7 @@ function step4(){
     console.log('junction table updates complete')
     res.send('complete')
 }//end step 4
+        }//end else for checking names are submitted
 
     }); //end REGISTRATION POST
 
